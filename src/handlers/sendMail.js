9014f3e91e96e7b57c7c290498bbf1ac661dff82
Sing-Kai Chiu <singkai.chiu@gmail.com>
2023-04-01 11:03:42 +0100

    first commit

---
+++ src/handlers/sendMail.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk')
+
+const ses = new AWS.SES({region: 'eu-west-1'})
+
+async function sendMail(event, context) {
+
+  const record = event.Records[0]
+
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body)
+  const {subject, body, recipient} = email;
+
+  const params = {
+    Source: 'singkai.chiu@gmail.com',
+    Destination: {
+      ToAddresses: [recipient],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body,
+        },
+      },
+      Subject: {
+        Data: subject,
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log('error sending email')
+    console.error(error);
+  }
+}
+
+exports.handler = sendMail;
+
+
